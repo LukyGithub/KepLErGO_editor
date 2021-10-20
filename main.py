5a0947cdb5b3f2bd4e4fd8b31507450ce7a2f0eb
LukyGithub <luky.salomon@gmail.com>
2021-10-20 14:38:14 +0200

    shefhubs

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame as py
+
 lineCol = [251, 197, 49]
 scrW = 1000
 scrH = 700
@@ -6,6 +7,7 @@ running = True
 pressing = False
 pointsX = []
 pointsY = []
+
 
 py.init()
 img = py.image.load("fll2.bmp")
@@ -24,14 +26,15 @@ while running:
         pointsX.append(py.mouse.get_pos()[0])
         pointsY.append(py.mouse.get_pos()[1])
         if len(pointsX) > 1:
-            py.draw.line(screen, lineCol, (pointsX[len(pointsX) - 1], pointsY[len(pointsY) - 1] ), (pointsX[len(pointsX) - 2], pointsY[len(pointsY) - 2]))
+            py.draw.aaline(screen, lineCol, (pointsX[len(pointsX) - 1], pointsY[len(pointsY) - 1] ), (pointsX[len(pointsX) - 2], pointsY[len(pointsY) - 2]))
     elif not py.mouse.get_pressed(3)[0]:
         pressing = False
     if py.mouse.get_pressed(3)[2]:
         for i in range(0, len(pointsX)):
             if(py.mouse.get_pos()[0] > pointsX[i] - 5 and py.mouse.get_pos()[0] < pointsX[i] + 5):
                 if(py.mouse.get_pos()[1] > pointsY[i] - 5 and py.mouse.get_pos()[1] < pointsY[i] + 5):
-                    print("Wery gut wetr!")
+                    rect1 = py.Rect(py.mouse.get_pos()[0], py.mouse.get_pos()[1], 50, 200)
+                    py.draw.rect(screen, (0, 0, 255), rect1)
             i = i + 1
         
     py.display.flip()
